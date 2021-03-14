13621e042884ac2eaf2e3a68121ba2ef48ca7559
h3h3boii <80233365+h3h3boii@users.noreply.github.com>
2021-03-14 21:03:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://project-bot-indonitro.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
